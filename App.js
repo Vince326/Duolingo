60600fd444e3a9dc1929994b8c26aafaa78497db
Vincent Hunter <jhearst29@gmail.com>
2023-07-11 20:52:50 -0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// const App = () => {
+
+//   return (
+//     <View style={{
+//       flex: 1, alignItems: "center", justifyContent: "center"
+//     }}>
+//       <Text>Hello World</Text>
+//     </View>
+//   );
+// };
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={{
+        flex: 1, alignItems: "center", justifyContent: "center"
+      }}>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
